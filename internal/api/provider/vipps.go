6cd5f7e9c5e2dd8f4d405c5afe14f8ca978cdd14
Martin Pedersen <martin@m4r7.in>
2025-09-26 15:55:03 +0200

    add more fields

--- internal/api/provider/vipps.go
+++ internal/api/provider/vipps.go
@@ -17,16 +17,66 @@ type vippsProvider struct {
 	APIPath string
 }
 
+type vippsAddress struct {
+	AddressType   string `json:"address_type"`
+	Country       string `json:"country"`
+	Formatted     string `json:"formatted"`
+	PostalCode    string `json:"postal_code"`
+	Region        string `json:"region"`
+	StreetAddress string `json:"street_address"`
+}
+
+type vippsDelegatedConsent struct {
+	Language             string `json:"language"`
+	Heading              string `json:"heading"`
+	TermsDescription     string `json:"termsDescription"`
+	ConfirmConsentButton string `json:"confirmConsentButtonText"`
+	Links                struct {
+		TermsLinkText            string `json:"termsLinkText"`
+		TermsLinkURL             string `json:"termsLinkUrl"`
+		PrivacyStatementLinkText string `json:"privacyStatementLinkText"`
+		PrivacyStatementLinkURL  string `json:"privacyStatementLinkUrl"`
+	} `json:"links"`
+	TimeOfConsent string `json:"timeOfConsent"`
+	Consents      []struct {
+		ID                  string `json:"id"`
+		Accepted            bool   `json:"accepted"`
+		Required            bool   `json:"required"`
+		TextDisplayedToUser string `json:"textDisplayedToUser"`
+	} `json:"consents"`
+}
+
+type vippsAgreement struct {
+	Language    string `json:"language"`
+	Description string `json:"description"`
+	ButtonText  string `json:"buttonText"`
+	Links       struct {
+		TermsLinkText            string `json:"termsLinkText"`
+		TermsLinkURL             string `json:"termsLinkUrl"`
+		PrivacyStatementLinkText string `json:"privacyStatementLinkText"`
+		PrivacyStatementLinkURL  string `json:"privacyStatementLinkUrl"`
+	} `json:"links"`
+	Timestamp string `json:"timestamp"`
+	Accepted  bool   `json:"accepted"`
+}
+
+// https://developer.vippsmobilepay.com/api/login/#operation/userinfoAuthorizationCode
 type vippsUser struct {
-	FamilyName string `json:"family_name"`
-	GivenName  string `json:"given_name"`
-	Name       string `json:"name"`
-	Email      string `json:"email"`
-	Sid        string `json:"sid"`
-	Sub        string `json:"sub"`
-	Birthdate  string `json:"birthdate"`
-	Phone      string `json:"phone_number"`
-	Gender     string `json:"gender"`
+	FamilyName          string                  `json:"family_name"`
+	GivenName           string                  `json:"given_name"`
+	Name                string                  `json:"name"`
+	Email               string                  `json:"email"`
+	EmailVerified       bool                    `json:"email_verified"`
+	Sid                 string                  `json:"sid"`
+	Sub                 string                  `json:"sub"`
+	Birthdate           string                  `json:"birthdate"`
+	PhoneNumber         string                  `json:"phone_number"`
+	PhoneNumberVerified bool                    `json:"phone_number_verified"`
+	Gender              string                  `json:"gender"`
+	Address             []vippsAddress          `json:"address"`
+	OtherAddresses      []vippsAddress          `json:"other_addresses"`
+	DelegatedConsents   []vippsDelegatedConsent `json:"delegated_consents"`
+	Agreement           []vippsAgreement        `json:"agreement"`
 }
 
 func NewVippsProvider(ext conf.OAuthProviderConfiguration, scopes string) (OAuthProvider, error) {
@@ -78,10 +128,17 @@ func (g vippsProvider) GetUserData(ctx context.Context, tok *oauth2.Token) (*Use
 			PreferredUsername: u.Name,
 			FullName:          u.Name,
 			Email:             strings.ToLower(u.Email),
+			EmailVerified:     u.EmailVerified,
 			Birthdate:         u.Birthdate,
-			Phone:             u.Phone,
-			PhoneVerified:     u.Phone != "", // Vipps phone numbers are always verified
+			Phone:             u.PhoneNumber,
+			PhoneVerified:     u.PhoneNumberVerified,
 			Gender:            u.Gender,
+			CustomClaims: map[string]any{
+				"address":            u.Address,
+				"other_addresses":    u.OtherAddresses,
+				"delegated_consents": u.DelegatedConsents,
+				"agreement":          u.Agreement,
+			},
 		},
 		Emails: []Email{{
 			Email:    u.Email,
